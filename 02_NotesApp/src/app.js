1e025b9b1123fbc2678a30411a66fec4ee212297
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-22 20:08:00 +0500

    Add Postman resources and implement notes API endpoints

--- 02_NotesApp/src/app.js
+++ 02_NotesApp/src/app.js
@@ -18,10 +18,28 @@ const notes = [
 
 */
 
+app.use(express.json()) // a middleware 
 
 const notes = []
 
+app.post('/notes', (req,res)=>{
+    console.log(req.body);
+    notes.push(req.body)
 
+    res.status(201).json({
+        message : 'Note Created Successfully!!'
+    })
+
+    
+})
+
+
+app.get('/notes',(req,res) => {
+    res.status(200).json({
+        message:'Notes fetch succesfully!',
+        notes:notes 
+    })
+})
 
 module.exports = app
 
