864ed8d60a78f8f854d12a5390ec29408270a24a
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-23 14:51:07 +0500

    refactor: improve formatting and consistency in app.js for note routes

--- 03_MongoDB/src/app.js
+++ 03_MongoDB/src/app.js
@@ -6,19 +6,67 @@ app.use(express.json())
 
 
 
-app.post('/notes',async (req,res) => {
+app.post('/notes', async (req, res) => {
     const data = req.body
     await noteModel.create({
         title: data.title,
-        description : data.description
+        description: data.description
     })
     res.status(201).json({
-        message:'Note Created Successfully'
+        message: 'Note Created Successfully'
     })
 })
 
 
+app.get('/notes', async (req, res) => {
 
+
+    /**
+ * find() --> [{},{}] or []
+ * findOne() --> {} or null
+ */
+
+
+    const notes = await noteModel.find() // find always returns an array
+
+    // const notes = await noteModel.findOne({
+    //     title : 'test_title_1'
+    // })
+
+    res.status(201).json({
+        message: 'Notes Fetched Successfully!',
+        notes: notes
+    })
+})
+
+
+app.delete('/notes/:id', async (req, res) => {
+    const id = req.params.id
+    await noteModel.findOneAndDelete({
+        _id : id
+    })
+    res.status(200).json({
+        message: 'Notes Deleted Successfully!',
+        
+    })
+
+})
+
+
+app.patch('/notes/:id', async (req, res) => {
+    const id = req.params.id
+    const description = req.body.description
+    await noteModel.findOneAndUpdate({
+        _id : id
+    },{
+        description:description
+    })
+    res.status(200).json({
+        message: 'Notes Updated Successfully!',
+        
+    })
+
+})
 
 
 
