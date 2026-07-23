21457bfa428928d4fa3b55602db650ed9b6d52a8
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-23 19:23:54 +0500

    refactor: update storage service to use ImageKit client for file uploads

--- 04_Fullstack/src/services/storage.service.js
+++ 04_Fullstack/src/services/storage.service.js
@@ -1,24 +1,17 @@
-const ImageKit = require('@imagekit/nodejs')
+const ImageKit = require("@imagekit/nodejs").default;
+const { toFile } = require("@imagekit/nodejs");
 
-
-const imagekit = new ImageKit({
-    privateKey: 'private_MeSrWb310Sot7Xou/BLQwenD6+0=',
-    publicKey: "public_47FgbRIibQ5ChoA25ays2GfrmjA="
-})
-
+const client = new ImageKit({
+  privateKey: "private_MeSrWb310Sot7Xou/BLQwenD6+0=",
+});
 
 async function uploadFile(buffer) {
-    const result = await imagekit.client.upload({
-        file:buffer,
-        fileName : "image.jpg"
-    })
-    return result
-    
+  const result = await client.files.upload({
+    file: await toFile(buffer, "image.jpg"),
+    fileName: "image.jpg",
+  });
+
+  return result;
 }
 
-
-module.exports = uploadFile
-
-
-
-
+module.exports = uploadFile;
