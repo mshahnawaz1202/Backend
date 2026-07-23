272f5d7c72e8aa8b0c230e37caffa2a452358c80
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-23 20:04:37 +0500

    feat: initialize backend with Express, MongoDB, and ImageKit integration
    - created package.json with necessary dependencies
    - implemented server setup in server.js
    - added Express app configuration in app.js
    - created MongoDB connection logic in db.js
    - defined post model schema in post.model.js
    - implemented file upload service using ImageKit in storage.service.js
    - added endpoint to create posts and fetch all posts

--- 04_Fullstack/backend/src/app.js
+++ 04_Fullstack/backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const multer = require('multer')
 const uploadFile = require('./services/storage.service')
+const postModel = require('./models/post.model')
+
 const app = express()
 
 
@@ -15,9 +17,28 @@ app.post('/create-post',upload.single("image") , async (req,res) => {
 
     const result = await uploadFile(req.file.buffer)
     console.log(result)
+
+    const post = await postModel.create({
+        image:result.url,
+        caption:req.body.caption
+    })
+
+    return res.status(201).json({
+        message: "Post Created Successfully!",
+        post
+    })
+
     
 })
 
+
+app.get('/posts',async (req,res) => {
+    const posts = await postModel.find()
+    return res.status(200).json({
+        message : "Posts Fetched Successfully!",
+        posts
+    })
+})
 
 
 
