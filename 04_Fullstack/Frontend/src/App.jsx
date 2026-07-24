cbcd0eb6a2f1de89cdfb301ae32a81482951b3fb
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-24 11:45:40 +0500

    feat: implement routing with CreatePost and Feed components

--- 04_Fullstack/Frontend/src/App.jsx
+++ 04_Fullstack/Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import CreatePost from './components/CreatePost'
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Router>
+      <Routes>
+        <Route path='/create-post' element= {<CreatePost/>} />
+        <Route path='/about' element= {<h1>About</h1>} />
+      </Routes>
+    </Router>
   )
 }
 
