e05aed32893756718d2a5c70776d18bcb5226c09
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-24 12:01:12 +0500

    feat: add Feed component and update routing in App.jsx style: implement styles for Feed section and post cards in index.css

--- 04_Fullstack/Frontend/src/App.jsx
+++ 04_Fullstack/Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import CreatePost from './components/CreatePost'
+import Feed from './components/Feed'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/create-post' element= {<CreatePost/>} />
-        <Route path='/about' element= {<h1>About</h1>} />
+        <Route path='/feed' element= {<Feed/>} />
       </Routes>
     </Router>
   )
