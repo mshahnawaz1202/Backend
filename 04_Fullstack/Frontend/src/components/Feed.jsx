b0da005b0d3450d20d44715a7ea7347b7fb003ee
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-24 12:03:03 +0500

    Frontend completed!

--- 04_Fullstack/Frontend/src/components/Feed.jsx
+++ 04_Fullstack/Frontend/src/components/Feed.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
 
+
 function Feed() {
     const [posts] = useState([
         {
             _id: 1,
             image:
                 "https://images.unsplash.com/photo-1784296868267-e8ac423c7afa?q=80&w=1206&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            caption: "Beautiful Nature 🌿",
+            caption: "Beautiful Nature ",
         },
         {
             _id: 2,
