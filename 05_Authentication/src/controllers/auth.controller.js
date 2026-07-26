c6bfdee1b9915c78ade7d2dbfca657f59e329277
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-26 13:30:18 +0500

    docs: clarify comment on user registration error handling for unique email constraint

--- 05_Authentication/src/controllers/auth.controller.js
+++ 05_Authentication/src/controllers/auth.controller.js
@@ -30,7 +30,7 @@ async function register(req, res) {
 
     /** Problem in above code  is that user can make multiple accounts with same username and email which is not good 
      * so first we do changes in model and add unique in email and
-     * then error handling in controller
+     * then error handling in controller before a user created 
     */
 
 
