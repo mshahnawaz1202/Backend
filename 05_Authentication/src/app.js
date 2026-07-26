33e6e75b6c69188c40c35c026a2ae0246955e456
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-26 18:00:45 +0500

    feat: add post routes to the application

--- 05_Authentication/src/app.js
+++ 05_Authentication/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const authRoutes = require('./routes/auth.routes')
+const postRoutes = require('./routes/post.routes')
 const cookieParser = require('cookie-parser')
 
 
@@ -10,6 +11,8 @@ app.use(cookieParser())
 
 
 app.use('/api/auth',authRoutes)
+app.use('/api/posts',postRoutes)
 
 
-module.exports = app
+
+module.exports = app 
